cf4dc9d96711a9d1268c11903dd057a9947f99d5
CalvinDoesCS <calvinntran.cs@gmail.com>
2024-08-16 20:43:45 -0400

    Rebuilt Logic for typewriter

--- build/animationTyper.js
+++ build/animationTyper.js
@@ -1,54 +1,29 @@
-let animationTimeout;
-function typeWriter(txt, speed, i) {
-    return new Promise(resolve => {
-        function type() {
-            if (i < txt.length) {
-                document.getElementById("job-text").innerHTML += txt.charAt(i);
-                i++;
-                animationTimeout = setTimeout(type, speed);
-            } else {
-                resolve();
-            }
-        }
-        type();
-    });
+function sleep(ms){
+    return new Promise((resolve)=> setTimeout(resolve,ms));
 }
+const phrases = ["Frontend Development", "Backend Developement", "Fullstack Development", "Machine Learning"];
+const el = document.getElementById("job-text");
 
-function typeDeleter(speed) {
-    return new Promise(resolve => {
-        function deleteText() {
-            let txt = document.getElementById("job-text").innerHTML;
-            if (txt.length > 0) {
-                document.getElementById("job-text").innerHTML = txt.slice(0, -1);
-                animationTimeout = setTimeout(deleteText, speed);
-            } else {
-                resolve();
-            }
+let sleepTime = 100;
+
+let currPhraseIndex = 0;
+
+const writeLoop = async () => {
+    while(true){
+        let currWord = phrases[currPhraseIndex % phrases.length];
+
+        for(let i = 0; i < currWord.length; i++){
+            el.innerText = currWord.substring(0,i+1);
+            await sleep(sleepTime);
         }
-        deleteText();
-    });
-}
-
-async function animationTyper() {
-    clearAnimation();
-    try {
-        let job_titles = ['Frontend Development', 'Backend Development', 'Fullstack Development', 'Machine Learning'];
-
-        for (let index = 0; index < job_titles.length; index++) {
-            await typeWriter(job_titles[index], 50, 0);
-            await new Promise(resolve => setTimeout(resolve, 500)); // Adjust the delay between typing and deletion
-            await typeDeleter(50);
-            await new Promise(resolve => setTimeout(resolve, 500)); // Adjust the delay after deletion
+        await sleep(sleepTime * 10);
+        for(let i = currWord.length - 1; i > 0; i--){
+            el.innerText = currWord.substring(0,i-1);
+            await sleep(sleepTime);
         }
-    } catch (error) {
-        console.error('Error in animationTyper:', error);
+        await sleep(sleepTime * 5);
+
+        currPhraseIndex += 1
     }
 }
-
-function clearAnimation() {
-    document.getElementById("job-text").innerHTML = "";
-    clearTimeout(animationTimeout);
-}
-animationTyper();
-setInterval(animationTyper, 13400);
-
+writeLoop();
